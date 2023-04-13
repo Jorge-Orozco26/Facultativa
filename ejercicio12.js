cf10787342653ba5ebd46b01dc9675eff5587a70
Jorge Orozco <creneorozco@gmail.com>
2023-04-13 08:33:05 -0600

    Ejercicios de facultativa

---
+++ ejercicio12.js
@@ -0,0 +1,5 @@
+function exe12(arr) {
+    return arr[arr.lenght-1]
+}
+
+document.write(exe12)
---
+++ ejercicio31.js
@@ -0,0 +1,11 @@
+function exe31(n){
+    let suma=0
+    for(let i=0; i<n.lenght;i++){
+        suma= suma + n[i]
+
+    }
+    return suma/n.lenght
+}
+
+exe31([8,10,20,30,14,16])
+
